aa5632a399b07ef391e8ce06fd7420df14e69881
bob <Mansalati18@gmail.com>
2024-03-18 13:08:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
 }
+
+describe('index.js', function () {
+  describe('scuberGreetingForFeet()', function () {
+    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+    });
+
+  });
+});
+
+function addFive(someNumber) {
+ 
+  let result
+  if (someNumber > 0) {
+    result = someNumber + 5;
+  }
+
+  return result
+}
+
+
+addFive(10);
+//=> 15
+
+addFive(20);
+//=> 25
+
+addFive(-5);
+//=> undefined
+
+addFive(addFive(5));
